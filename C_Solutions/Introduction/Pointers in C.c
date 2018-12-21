e9f7f05276da2ff30eb2d427dbbbe03d9d17748d
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-12-21 05:37:09 +0200

    Create Pointers in C.c

---
+++ C_Solutions/Introduction/Pointers in C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void update(int *a,int *b) {
+    int x = *a + *b ; 
+    int y = abs(*a-*b) ; 
+    *a = x ; 
+    *b = y ;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
